d4460bfea3e47cf28689e268035f9249fa4e648e
adsPim2019 <ads.pim2@gmail.com>
2019-03-21 23:37:59 -0300

    teste

--- SistemaColecionador/Conexão/Conexao.vb
+++ SistemaColecionador/Conexão/Conexao.vb
@@ -2,7 +2,7 @@
 
 Module Conexao
     'CRIAR CONEXAO COM BD SERVER
-    Public con As New SqlConnection("Server=DESKTOP-628SFJB;DataBase=SistemaColecionador;User=lucas;Password=699002")
+    Public con As New SqlConnection("Server=DESKTOP-LVOTD2H\SQLEXPRESS; DataBase=SistemaColecionador; Integrated Security = SSPI")
 
     'METODO ABRIR CONEXAO
     Sub abrir()
